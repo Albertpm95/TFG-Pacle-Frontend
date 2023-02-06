d2f03507fe74e956258168ef071f0ef569f6e934
Albert <albertpm95@outlook.com>
2023-02-06 12:31:02 +0100

    Added environments to gitignore

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-	production: false,
-	title: 'Entorno local',
-	apiURL: 'http://127.0.0.1:8000/'
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-	production: true,
-	title: 'Entorno produccion',
-	apiURL: 'http://127.0.0.1:8000/'
-};
-

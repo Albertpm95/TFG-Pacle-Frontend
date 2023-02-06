83fcedb83982c62905332b2d4dcdd2fec018b429
Albert <albertpm95@outlook.com>
2023-02-06 12:32:02 +0100

    gitignore

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'environments_2/environment';
+import { environment } from '@environments/environment';
 import { UserAction } from '@models/acciones-usuario';
 import { Observable } from 'rxjs';
 import { Usuario } from '../models/usuario';
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'environments_2/environment';
+import { environment } from '@environments/environment';
 
 
 import { Observable } from 'rxjs';

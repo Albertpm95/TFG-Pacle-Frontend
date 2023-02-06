d6ae273070d2b5ca5203024c69e4d4468d5d54c6
Albert <albertpm95@outlook.com>
2023-02-06 12:59:03 +0100

    Nuevo modelo de acta

---
+++ src/app/models/acta.ts
@@ -0,0 +1,7 @@
+export interface Acta {
+  id_acta: string;
+  lenguaje: 'Español' | 'English' | 'Català' | 'Français' | 'Chainese' | 'Deutsch';
+  tipo: 'Ordinaria' | 'Extraordinaria'
+  fecha: Date;
+  activa: boolean;
+}

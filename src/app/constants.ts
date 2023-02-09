5c00f5a9ecc96594cbb3dfb7023929306c216c1d
Albert <albertpm95@outlook.com>
2023-02-09 12:03:58 +0100

    Actualizadas rutas con constantes

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -4,10 +4,13 @@ export class Constants {
   static PORCIENTO = 100; // Para calcular el porcentaje
   static LENGUAJE_POR_DEFECTO = 'Español';
   static TIPO_POR_DEFECTO = 'Ordinaria';
-  static ACTA_CREATE = 'acta/create';
-  static ACTA_LIST = 'acta/list';
-  static ALUMNO_CREATE = 'alumno/create';
-  static ALUMNO_LIST = 'alumno/list';
-  static USUARIO_CREATE = 'usuario/create';
-  static USUARIO_LIST = 'usuario/list';
+  static ACTA = 'acta';
+  static ALUMNO = 'alumno';
+  static USUARIO = 'usuario';
+  static ACTA_CREATE = Constants.ACTA + '/create';
+  static ACTA_LIST = Constants.ACTA + '/list';
+  static ALUMNO_CREATE = Constants.ALUMNO + '/create';
+  static ALUMNO_LIST = Constants.ALUMNO + '/list';
+  static USUARIO_CREATE = Constants.USUARIO + '/create';
+  static USUARIO_LIST = Constants.USUARIO + '/list';
 }
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Constants } from '@constants';
+import { ActaModule } from './acta/acta.module';
+import { AlumnoModule } from './alumno/alumno.module';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   {
-    path: 'actas',
+    path: Constants.ACTA,
+    loadChildren: () => ActaModule,
   },
   {
-    path: 'usuarios',
+    path: Constants.USUARIO,
   },
   {
-    path: 'alumnos',
+    path: Constants.ALUMNO,
+    loadChildren: () => AlumnoModule,
   },
 ];
 

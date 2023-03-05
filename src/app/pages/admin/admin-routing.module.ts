764b7696d9708819040a892706313831ae50e8eb
Albert <albertpm95@outlook.com>
2023-03-05 18:27:05 +0100

    Added routing to components

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Constants, Routers } from '@constants';
 import { AdminComponent } from './admin.component';
+import { IdiomasComponent } from './idiomas/idiomas.component';
+import { TiposComponent } from './tipos/tipos.component';
 
 const routes: Routes = [
-  { path: Constants.IDIOMAS, component: AdminComponent, title: 'Idiomas' }];
+  { path: Constants.IDIOMAS, component: AdminComponent, title: 'Idiomas' },
+  { path: Constants.HORARIOS, component: IdiomasComponent, title: 'Idiomas' },
+  { path: Constants.TIPOS, component: TiposComponent, title: 'Idiomas' },
+  { path: '', component: AdminComponent, title: 'Panel de administracion' },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

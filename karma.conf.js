679a319b222ba76b8856323461cad2a63acf4d11
Albert <albertpm95@outlook.com>
2023-03-31 19:25:23 +0200

    Updated karma and proyect configuration

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-edge-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -30,7 +30,7 @@ module.exports = function (config) {
     reporters: ['progress', 'kjhtml'],
     colors: true,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Edge'],
     restartOnFileChange: false
   })
 }

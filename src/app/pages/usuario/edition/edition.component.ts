3650a5bc0628a37316bd55cff07a8cf6b362c8a2
Albert <albertpm95@outlook.com>
2023-03-31 19:02:03 +0200

    Created User details component

--- src/app/pages/usuario/edition/edition.component.ts
+++ src/app/pages/usuario/edition/edition.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { COMPONENTS, MODULES } from '@constants';
+import { Rol } from '@models/rol';
+import { Usuario } from '@models/usuario';
+import { ApiService } from '@services/api.service';
+import { Observable } from 'rxjs';
 
 @Component({
   templateUrl: './edition.component.html',
@@ -6,4 +13,36 @@ import { Component } from '@angular/core';
 })
 export class EditionComponent {
 
+  usuarioDetailsForm: FormGroup = new FormGroup('')
+  usuario: Usuario | undefined
+  loading: boolean = true
+  listRoute = '/' + MODULES.USUARIO + '/' + COMPONENTS.LIST
+
+  listaRolesUsuario$: Observable<Rol[]> = this.apiService.getRolesUsuario()
+
+  constructor(
+    private apiService: ApiService,
+    private formBuilder: FormBuilder,
+    private activactedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    let idUsuario = this.activactedRoute.snapshot.params['idUsuario']
+    idUsuario ? this.loadForm(idUsuario) : this.initializeForm()
+  }
+
+  private initializeForm(): void {
+    this.usuarioDetailsForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      apellidos: ['', Validators.required],
+      rol: ['', Validators.required],
+      estado: ['', Validators.required],
+      username: ['', Validators.required],
+    })
+    this.loading = false;
+  }
+
+  private loadForm(idUsuario: number): void {
+
+  }
 }

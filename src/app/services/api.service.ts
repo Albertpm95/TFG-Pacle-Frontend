c2a6c66f8a9e3e86fed3f02f3c166a6296f23508
Albert <albertpm95@outlook.com>
2023-04-03 09:26:58 +0200

    Removed mocked data

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -19,159 +19,147 @@ import { Usuario } from '@models/usuario'
 import { Observable, of } from 'rxjs'
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class ApiService {
-  apiUrl = environment.apiURL
+    apiUrl = environment.apiURL
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  /** Admin */
-  subirFicheroExcel(excel: File): Observable<File> {
-    return this.http.post<File>(
-      this.apiUrl + API_ENDPOINTS.ADMIN_UPLOAD_ALUMNOS,
-      excel,
-    )
-  }
-  getRolesUsuario(): Observable<Rol[]> {
-    return this.http.get<Rol[]>(this.apiUrl + API_ENDPOINTS.CONFIG_ROL_LIST)
-  }
-  /** ---------------------------------------------------------------------- */
+    /** Admin */
+    subirFicheroExcel(excel: File): Observable<File> {
+        return this.http.post<File>(
+            this.apiUrl + API_ENDPOINTS.ADMIN_UPLOAD_ALUMNOS,
+            excel,
+        )
+    }
+    getRolesUsuario(): Observable<Rol[]> {
+        return this.http.get<Rol[]>(this.apiUrl + API_ENDPOINTS.CONFIG_ROL_LIST)
+    }
+    /** ---------------------------------------------------------------------- */
 
-  /** Convocatorias */
-  getConvocatorias(): Observable<Convocatoria[]> {
-    return of(MockUpDB.convocatoriasMockUp)
-    return this.http.get<Convocatoria[]>(
-      this.apiUrl + API_ENDPOINTS.CONVOCATORIA_LIST,
-    )
-  }
-  getConvocatoria(idConvocatoria: number): Observable<Convocatoria> {
-    return of(MockUpDB.convocatoriaMockUp1)
-    return this.http.get<Convocatoria>(
-      this.apiUrl + API_ENDPOINTS.CONVOCATORIA_UPDATE + '/' + idConvocatoria,
-    )
-  }
-  cambiarEstadoConvocatoria(idConvocatoria: number, estado_nuevo: boolean) {
-    return this.http.patch<Convocatoria>(this.apiUrl, {
-      idConvocatoria,
-      estado_nuevo,
-    })
-  }
-  updateConvocatoria(convocatoria: Convocatoria): Observable<Convocatoria> {
-    return this.http.post<Convocatoria>(this.apiUrl + API_ENDPOINTS.CONVOCATORIA_UPDATE, { convocatoria })
-  }
-  /** ---------------------------------------------------------------------- */
+    /** Convocatorias */
+    getConvocatorias(): Observable<Convocatoria[]> {
+        return this.http.get<Convocatoria[]>(
+            this.apiUrl + API_ENDPOINTS.CONVOCATORIA_LIST,
+        )
+    }
+    getConvocatoria(idConvocatoria: number): Observable<Convocatoria> {
+        return this.http.get<Convocatoria>(
+            this.apiUrl + API_ENDPOINTS.CONVOCATORIA_UPDATE + '/' + idConvocatoria,
+        )
+    }
+    cambiarEstadoConvocatoria(idConvocatoria: number, estado_nuevo: boolean) {
+        return this.http.patch<Convocatoria>(this.apiUrl, {
+            idConvocatoria,
+            estado_nuevo,
+        })
+    }
+    updateConvocatoria(convocatoria: Convocatoria): Observable<Convocatoria> {
+        return this.http.post<Convocatoria>(this.apiUrl + API_ENDPOINTS.CONVOCATORIA_UPDATE, { convocatoria })
+    }
+    /** ---------------------------------------------------------------------- */
 
-  /** Configuracion */
-  getHorariosConvocatoria(): Observable<Horario[]> {
-    return of(MockUpDB.horariosMockUp)
-    return this.http.get<Horario[]>(
-      this.apiUrl + API_ENDPOINTS.CONFIG_HORARIO_LIST,
-    )
-  }
-  addHorarioConvocatoria(horario: string): Observable<Horario> {
-    return this.http.put<Horario>(this.apiUrl + API_ENDPOINTS.CONFIG_HORARIO_CREATE, { horario })
-  }
-  deleteHorarioConvocatoria(idHorario: number): Observable<Horario> {
-    return this.http.delete<Horario>(this.apiUrl + API_ENDPOINTS.CONFIG_HORARIO_DELETE + idHorario)
-  }
-  getLenguajesConvocatoria(): Observable<Lenguaje[]> {
-    return of(MockUpDB.lenguajesMockUp)
-    return this.http.get<Lenguaje[]>(
-      this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_LIST,
-    )
-  }
-  addLenguajeConvocatoria(lenguaje: string): Observable<Lenguaje> {
-    return this.http.put<Lenguaje>(this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_CREATE, { lenguaje })
-  }
-  deleteLenguajeConvocatoria(idLenguaje: number): Observable<Lenguaje> {
-    return this.http.delete<Lenguaje>(this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_DELETE + idLenguaje)
-  }
-  getNivelesConvocatoria(): Observable<Nivel[]> {
-    return of(MockUpDB.nivelesMockUp)
-    return this.http.get<Nivel[]>(
-      this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_LIST,
-    )
-  }
-  addNivelConvocatoria(nivel: string): Observable<Nivel> {
-    return this.http.put<Nivel>(this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_CREATE, { nivel })
-  }
-  deleteNivelConvocatoria(idNivel: number): Observable<Nivel> {
-    return this.http.delete<Nivel>(this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_DELETE + idNivel)
-  }
-  getGenerosAlumno(): Observable<Genero[]> {
-    return of(MockUpDB.generosMockUp)
-    return this.http.get<Genero[]>(
-      this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_LIST,
-    )
-  }
-  addGeneroAlumno(genero: string): Observable<Genero> {
-    return this.http.put<Genero>(this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_CREATE, { genero })
-  }
-  deleteGeneroAlumno(idGenero: number): Observable<Genero> {
-    return this.http.delete<Genero>(this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_DELETE + idGenero)
-  }
-  /** ---------------------------------------------------------------------- */
+    /** Configuracion */
+    getHorariosConvocatoria(): Observable<Horario[]> {
+        return this.http.get<Horario[]>(
+            this.apiUrl + API_ENDPOINTS.CONFIG_HORARIO_LIST,
+        )
+    }
+    addHorarioConvocatoria(horario: string): Observable<Horario> {
+        return this.http.put<Horario>(this.apiUrl + API_ENDPOINTS.CONFIG_HORARIO_CREATE, { horario })
+    }
+    deleteHorarioConvocatoria(idHorario: number): Observable<Horario> {
+        return this.http.delete<Horario>(this.apiUrl + API_ENDPOINTS.CONFIG_HORARIO_DELETE + idHorario)
+    }
+    getLenguajesConvocatoria(): Observable<Lenguaje[]> {
+        return this.http.get<Lenguaje[]>(
+            this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_LIST,
+        )
+    }
+    addLenguajeConvocatoria(lenguaje: string): Observable<Lenguaje> {
+        return this.http.put<Lenguaje>(this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_CREATE, { lenguaje })
+    }
+    deleteLenguajeConvocatoria(idLenguaje: number): Observable<Lenguaje> {
+        return this.http.delete<Lenguaje>(this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_DELETE + idLenguaje)
+    }
+    getNivelesConvocatoria(): Observable<Nivel[]> {
+        return this.http.get<Nivel[]>(
+            this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_LIST,
+        )
+    }
+    addNivelConvocatoria(nivel: string): Observable<Nivel> {
+        return this.http.put<Nivel>(this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_CREATE, { nivel })
+    }
+    deleteNivelConvocatoria(idNivel: number): Observable<Nivel> {
+        return this.http.delete<Nivel>(this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_DELETE + idNivel)
+    }
+    getGenerosAlumno(): Observable<Genero[]> {
+        return this.http.get<Genero[]>(
+            this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_LIST,
+        )
+    }
+    addGeneroAlumno(genero: string): Observable<Genero> {
+        return this.http.put<Genero>(this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_CREATE, { genero })
+    }
+    deleteGeneroAlumno(idGenero: number): Observable<Genero> {
+        return this.http.delete<Genero>(this.apiUrl + API_ENDPOINTS.CONFIG_IDIOMA_DELETE + idGenero)
+    }
+    /** ---------------------------------------------------------------------- */
 
-  /** Actas */
-  getActas(): Observable<Acta[]> {
-    return of(MockUpDB.actasMockUp)
-    return this.http.get<Acta[]>(this.apiUrl + API_ENDPOINTS.ACTA_LIST)
-  }
-  getActaID(idActa: number): Observable<Acta> {
-    return this.http.get<Acta>(this.apiUrl + API_ENDPOINTS.ACTA_UPDATE + idActa)
-  }
-  /** ---------------------------------------------------------------------- */
+    /** Actas */
+    getActas(): Observable<Acta[]> {
+        return this.http.get<Acta[]>(this.apiUrl + API_ENDPOINTS.ACTA_LIST)
+    }
+    getActaID(idActa: number): Observable<Acta> {
+        return this.http.get<Acta>(this.apiUrl + API_ENDPOINTS.ACTA_UPDATE + idActa)
+    }
+    /** ---------------------------------------------------------------------- */
 
-  /** Comprension */
-  updateComprensionLectora(comprensionLectora: ComprensionLectora): Observable<ComprensionLectora> {
-    return this.http.patch<ComprensionLectora>(this.apiUrl + API_ENDPOINTS.EXPRESION_UPDATE, { comprensionLectora })
-  }
+    /** Comprension */
+    updateComprensionLectora(comprensionLectora: ComprensionLectora): Observable<ComprensionLectora> {
+        return this.http.patch<ComprensionLectora>(this.apiUrl + API_ENDPOINTS.EXPRESION_UPDATE, { comprensionLectora })
+    }
 
-  updateComprensionAuditiva(comprensionAuditiva: ComprensionAuditiva): Observable<ComprensionAuditiva> {
-    return this.http.patch<ComprensionAuditiva>(this.apiUrl + API_ENDPOINTS.EXPRESION_UPDATE, { comprensionAuditiva })
-  }
-  /** ---------------------------------------------------------------------- */
+    updateComprensionAuditiva(comprensionAuditiva: ComprensionAuditiva): Observable<ComprensionAuditiva> {
+        return this.http.patch<ComprensionAuditiva>(this.apiUrl + API_ENDPOINTS.EXPRESION_UPDATE, { comprensionAuditiva })
+    }
+    /** ---------------------------------------------------------------------- */
 
-  /** Expresion */
-  updateExpresionEscrita(expresionEscrita: ExpresionEscrita): Observable<ExpresionEscrita> {
-    return this.http.patch<ExpresionEscrita>(this.apiUrl + API_ENDPOINTS.EXPRESION_UPDATE, { expresionEscrita })
-  }
-  updateExpresionOral(expresionOral: ExpresionOral): Observable<ExpresionOral> {
-    return this.http.patch<ExpresionOral>(this.apiUrl + API_ENDPOINTS.EXPRESION_UPDATE, { expresionOral })
-  }
-  /** ---------------------------------------------------------------------- */
+    /** Expresion */
+    updateExpresionEscrita(expresionEscrita: ExpresionEscrita): Observable<ExpresionEscrita> {
+        return this.http.patch<ExpresionEscrita>(this.apiUrl + API_ENDPOINTS.EXPRESION_UPDATE, { expresionEscrita })
+    }
+    updateExpresionOral(expresionOral: ExpresionOral): Observable<ExpresionOral> {
+        return this.http.patch<ExpresionOral>(this.apiUrl + API_ENDPOINTS.EXPRESION_UPDATE, { expresionOral })
+    }
+    /** ---------------------------------------------------------------------- */
 
-  /** Alumnos */
-  getAlumnos(): Observable<Alumno[]> {
-    return of(MockUpDB.alumnosMockUp)
-    return this.http.get<Alumno[]>(this.apiUrl + API_ENDPOINTS.ALUMNO_LIST)
-  }
-  getAlumnosConvocatoria(idConvocatoria: number): Observable<Alumno[]> {
-    return of(MockUpDB.alumnosMockUp)
-    return this.http.get<Alumno[]>(this.apiUrl + API_ENDPOINTS.ALUMNO_LIST + idConvocatoria)
-  }
-  getAlumnoID(idAlumno: number): Observable<Alumno> {
-    return of(MockUpDB.alumnoMockUp1)
-    return this.http.get<Alumno>(this.apiUrl + API_ENDPOINTS.ALUMNO_UPDATE + '/' + idAlumno)
-  }
-  updateAlumno(alumno: Alumno): Observable<Alumno> {
-    return this.http.post<Alumno>(this.apiUrl + API_ENDPOINTS.ALUMNO_UPDATE, { alumno })
-  }
-  /** ---------------------------------------------------------------------- */
+    /** Alumnos */
+    getAlumnos(): Observable<Alumno[]> {
+        return this.http.get<Alumno[]>(this.apiUrl + API_ENDPOINTS.ALUMNO_LIST)
+    }
+    getAlumnosConvocatoria(idConvocatoria: number): Observable<Alumno[]> {
+        return this.http.get<Alumno[]>(this.apiUrl + API_ENDPOINTS.ALUMNO_LIST + idConvocatoria)
+    }
+    getAlumnoID(idAlumno: number): Observable<Alumno> {
+        return this.http.get<Alumno>(this.apiUrl + API_ENDPOINTS.ALUMNO_UPDATE + '/' + idAlumno)
+    }
+    updateAlumno(alumno: Alumno): Observable<Alumno> {
+        return this.http.post<Alumno>(this.apiUrl + API_ENDPOINTS.ALUMNO_UPDATE, { alumno })
+    }
+    /** ---------------------------------------------------------------------- */
 
-  /** Usuarios */
-  getUsuarios(): Observable<Usuario[]> {
-    return of(MockUpDB.usuariosMockUp)
-    return this.http.get<Usuario[]>(this.apiUrl + API_ENDPOINTS.USUARIO_LIST)
-  }
-  getUsuarioID(
-    idUsuario?: string,
-  ): Observable<Usuario> {
-    return of(MockUpDB.usuarioMockUp1)
-    return this.http.get<Usuario>(
-      this.apiUrl + API_ENDPOINTS.USUARIO_UPDATE + { idUsuario },
-    )
-  }
-  /** ---------------------------------------------------------------------- */
+    /** Usuarios */
+    getUsuarios(): Observable<Usuario[]> {
+        return this.http.get<Usuario[]>(this.apiUrl + API_ENDPOINTS.USUARIO_LIST)
+    }
+    getUsuarioID(
+        idUsuario?: string,
+    ): Observable<Usuario> {
+        return this.http.get<Usuario>(
+            this.apiUrl + API_ENDPOINTS.USUARIO_UPDATE + { idUsuario },
+        )
+    }
+    /** ---------------------------------------------------------------------- */
 }

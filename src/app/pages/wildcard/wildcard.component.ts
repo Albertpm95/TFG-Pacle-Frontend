ab7454f60a7fd3f1259ecaadfbc6adae4de3ea13
Albert <albertpm95@outlook.com>
2023-04-04 13:56:06 +0200

    Returned to scss

--- src/app/pages/wildcard/wildcard.component.ts
+++ src/app/pages/wildcard/wildcard.component.ts
@@ -1,14 +1,6 @@
 import { Component } from '@angular/core';
-
 @Component({
-  template: `
-    <h1> Pagina no encontrada </h1>
-  `
-  ,
-  styles: ['h1{ font-size: 1rem;}']
-
-
+    template: `<h1>Pagina no encontrada</h1>`,
+    styles: ['h1{ font-size: 1rem;}']
 })
-export class WildcardComponent {
-
-}
+export class WildcardComponent { }

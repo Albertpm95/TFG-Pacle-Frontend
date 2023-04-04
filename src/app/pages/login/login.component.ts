2e7e4a7671e180535a83ab2e03be40a6a09ec00c
Albert <albertpm95@outlook.com>
2023-04-04 08:53:22 +0200

    Updated login component with tests

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,46 +1,49 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router'
-import { API_ENDPOINTS, COMPONENTS, MODULES } from '@constants'
+import { COMPONENTS, MODULES } from '@constants'
 import { AuthService } from '@services/auth.service'
+import { StorageService } from '@services/storage.service'
 
 @Component({
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup = new FormGroup({})
-  loading: boolean = false
+    loginForm: FormGroup = new FormGroup({})
+    loading: boolean = false
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private authService: AuthService,
-    private router: Router,
-  ) { }
+    constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router, private storageService: StorageService) { }
 
-  ngOnInit(): void {
-    this.inicializarForm()
-  }
-
-  private inicializarForm(): void {
-    this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required],
-    })
-  }
-  public login(): void {
-    this.loading = true
-    if (this.loginForm.status == 'VALID') {
-      let login_form_data = new FormData()
-      login_form_data.append('username', this.loginForm.value.username)
-      login_form_data.append('password', this.loginForm.value.password)
-      this.authService.login(login_form_data)//.subscribe((data) => {        this.router.navigateByUrl(COMPONENTS.MENU)      })
+    ngOnInit(): void {
+        this.inicializarForm()
     }
-    this.router.navigateByUrl(MODULES.CONVOCATORIA + '/' + COMPONENTS.LIST) // TODO Remove
-  }
 
-  ngOnDestroy() {
-    this.loading = true
-  }
+    private inicializarForm(): void {
+        this.loginForm = this.formBuilder.group({
+            username: ['', [Validators.minLength(6), Validators.maxLength(12), Validators.pattern('^[a-zA-Z][a-zA-Z0-9_]+$')]],
+            password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(24), Validators.pattern('^[a-zA-Z][a-zA-Z0-9_]+$')]],
+        })
+    }
+
+    public login(): void {
+        if (this.loginForm.valid) {
+            this.loading = true
+            let loginFormData = new FormData()
+            loginFormData.append('username', this.loginForm.value.username)
+            loginFormData.append('password', this.loginForm.value.password)
+            this.authService.login(loginFormData).subscribe({
+                next: data => {
+                    this.storageService.saveUser(data)
+                    this.loading = false
+                    this.router.navigateByUrl(MODULES.CONVOCATORIA + '/' + COMPONENTS.LIST)
+                }, error: err => {
+                    this.storageService.clean()
+                    this.loading = false
+                }
+            })
+        }
+    }
+    ngOnDestroy() { }
 }

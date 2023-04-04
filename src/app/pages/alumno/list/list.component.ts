9b917833523f092874075e56037fb22971a84a9e
Albert <albertpm95@outlook.com>
2023-04-04 09:25:14 +0200

    Updated functionality in Alumno list to filter by convocatoria

--- src/app/pages/alumno/list/list.component.ts
+++ src/app/pages/alumno/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router';
 import { COMPONENTS, MODULES } from '@constants';
 import { Alumno } from '@models/alumno';
 import { ApiService } from '@services/api.service';
@@ -15,14 +16,26 @@ export class ListComponent {
     list_loaded: boolean = false;
     edit_route = '/' + MODULES.ALUMNO + '/' + COMPONENTS.EDITION
 
-    constructor(private apiService: ApiService) { }
+    constructor(private apiService: ApiService, private activactedRoute: ActivatedRoute) { }
 
     ngOnInit() {
-        this.initializeList()
+        let idConvocatoria = this.activactedRoute.snapshot.params['idConvocatoria']
+        idConvocatoria ? this.initializeFilteredListConvocatoria(idConvocatoria) : this.initializeList()
     }
 
     private initializeList(): void {
-        this.apiService.getAlumnos().subscribe((alumnos) => {
+        this.apiService.getAlumnos().subscribe((alumnos: Alumno[]) => {
+            console.log(alumnos)
+            if (alumnos) {
+                this.data_source = new MatTableDataSource(alumnos)
+                if (alumnos.length)
+                    this.list_loaded = true
+            }
+        })
+    }
+
+    private initializeFilteredListConvocatoria(idConvocatoria: number): void {
+        this.apiService.getAlumnosConvocatoria(idConvocatoria).subscribe((alumnos: Alumno[]) => {
             console.log(alumnos)
             if (alumnos) {
                 this.data_source = new MatTableDataSource(alumnos)

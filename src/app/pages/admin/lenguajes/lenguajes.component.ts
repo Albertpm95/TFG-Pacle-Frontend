cb77fca2b93a834036a5cf343c65ba61d1eac06b
Albert <albertpm95@outlook.com>
2023-04-11 18:58:28 +0200

    Fixed lenguaje

--- src/app/pages/admin/lenguajes/lenguajes.component.ts
+++ src/app/pages/admin/lenguajes/lenguajes.component.ts
@@ -1,30 +1,46 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Lenguaje } from '@models/lenguaje';
-import { ApiService } from '@services/api.service';
-import { Observable, Subject, takeUntil } from 'rxjs';
+import { Component } from '@angular/core'
+import { FormControl, RequiredValidator, Validators } from '@angular/forms'
+import { Lenguaje } from '@models/lenguaje'
+import { ApiService } from '@services/api.service'
+import { Observable, Subject, takeUntil } from 'rxjs'
 
 @Component({
     selector: 'app-lenguajes',
     templateUrl: './lenguajes.component.html',
-    styleUrls: ['./lenguajes.component.scss']
+    styleUrls: ['./lenguajes.component.scss'],
 })
 export class LenguajesComponent {
-
     nuevoLenguajeForm = new FormControl()
-    lenguajes$: Observable<Lenguaje[]> = this.apiService.getLenguajesConvocatoria()
+    lenguajes$: Observable<Lenguaje[]> =
+        this.apiService.getLenguajesConvocatoria()
 
     private destroy$: Subject<boolean> = new Subject<boolean>()
 
     constructor(private apiService: ApiService) { }
 
+    ngOnInit() {
+        this.nuevoLenguajeForm.setValidators([Validators.required])
+    }
+
     public deleteLenguajeConvocatoria(idLenguaje: number) {
-        idLenguaje ? this.apiService.deleteLenguajeConvocatoria(idLenguaje) : ''
+        idLenguaje ? this.apiService.deleteLenguajeConvocatoria(idLenguaje).pipe(takeUntil(this.destroy$)).subscribe(response => { console.log(response) }) : ''
     }
 
     public addLenguajeConvocatoria() {
-        this.nuevoLenguajeForm.valid ? this.apiService.addLenguajeConvocatoria(this.nuevoLenguajeForm.value).pipe(takeUntil(this.destroy$)).subscribe() : ''
+        if (this.nuevoLenguajeForm.valid) {
+            let lenguaje_nuevo: Lenguaje = { lenguaje: this.nuevoLenguajeForm.value }
+            this.apiService
+                .addLenguajeConvocatoria(lenguaje_nuevo)
+                .pipe(takeUntil(this.destroy$))
+                .subscribe(
+                    response => {
+                        console.log(response)
+                        this.nuevoLenguajeForm.reset()
+                    }
+                )
+        }
     }
+
     ngOnDestroy() {
         this.destroy$.next(true)
     }

1bacf92897812f9f09ccb168c142f4b271705ccf
albertpm95 <albertpm95@outlook.com>
2023-04-14 13:29:35 +0200

    Aded validator required

--- src/app/pages/admin/colectivouv/colectivouv.component.ts
+++ src/app/pages/admin/colectivouv/colectivouv.component.ts
@@ -1,61 +1,66 @@
 import { Component } from '@angular/core'
-import { FormControl } from '@angular/forms'
+import { FormControl, Validators } from '@angular/forms'
 import { ColectivoUV } from '@models/colectivouv'
 import { ApiService } from '@services/api.service'
 import { Subject, takeUntil } from 'rxjs'
 
 @Component({
-    selector: 'app-colectivouv',
-    templateUrl: './colectivouv.component.html',
-    styleUrls: ['./colectivouv.component.scss'],
+  selector: 'app-colectivouv',
+  templateUrl: './colectivouv.component.html',
+  styleUrls: ['./colectivouv.component.scss'],
 })
 export class ColectivouvComponent {
-    nuevoColectivoUVForm = new FormControl()
-    colectivosUV: ColectivoUV[] = []
+  nuevoColectivoUVForm = new FormControl()
+  colectivosUV: ColectivoUV[] = []
 
-    private destroy$: Subject<boolean> = new Subject<boolean>()
+  private destroy$: Subject<boolean> = new Subject<boolean>()
 
-    constructor(private apiService: ApiService) {
-        this.apiService
-            .getColectivosUV()
-            .pipe(takeUntil(this.destroy$))
-            .subscribe((response: ColectivoUV[]) => { this.colectivosUV = response })
+  constructor(private apiService: ApiService) {
+    this.nuevoColectivoUVForm.setValidators(Validators.required)
+    this.apiService
+      .getColectivosUV()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((response: ColectivoUV[]) => {
+        this.colectivosUV = response
+      })
+  }
+
+  public deleteColectivoUV(idColectivoUV: number) {
+    idColectivoUV
+      ? this.apiService
+          .deleteColectivoUV(idColectivoUV)
+          .pipe(takeUntil(this.destroy$))
+          .subscribe(() => {
+            let indexAEliminar = this.colectivosUV.findIndex(
+              (colectivo) => colectivo.idColectivoUV === idColectivoUV,
+            )
+            if (indexAEliminar != -1)
+              this.colectivosUV.splice(
+                this.colectivosUV.findIndex(
+                  (colectivo) => colectivo.idColectivoUV === idColectivoUV,
+                ),
+                1,
+              )
+          })
+      : ''
+  }
+
+  public addColectivoUV() {
+    if (this.nuevoColectivoUVForm.valid) {
+      let colectivoUV: ColectivoUV = {
+        colectivoUV: this.nuevoColectivoUVForm.value,
+      }
+      this.apiService
+        .addColectivoUV(colectivoUV)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe((response: ColectivoUV) => {
+          this.nuevoColectivoUVForm.reset()
+          this.colectivosUV.push(response)
+        })
     }
+  }
 
-    public deleteColectivoUV(idColectivoUV: number) {
-        idColectivoUV
-            ? this.apiService
-                .deleteColectivoUV(idColectivoUV)
-                .pipe(takeUntil(this.destroy$))
-                .subscribe(() => {
-                    let indexAEliminar = this.colectivosUV.findIndex(
-                        (colectivo) => colectivo.idColectivoUV === idColectivoUV,
-                    )
-                    if (indexAEliminar != -1)
-                        this.colectivosUV.splice(
-                            this.colectivosUV.findIndex(
-                                (colectivo) => colectivo.idColectivoUV === idColectivoUV,
-                            ),
-                            1,
-                        )
-                })
-            : ''
-    }
-
-    public addColectivoUV() {
-        if (this.nuevoColectivoUVForm.valid) {
-            let colectivoUV: ColectivoUV = { colectivoUV: this.nuevoColectivoUVForm.value }
-            this.apiService
-                .addColectivoUV(colectivoUV)
-                .pipe(takeUntil(this.destroy$))
-                .subscribe((response: ColectivoUV) => {
-                    this.nuevoColectivoUVForm.reset()
-                    this.colectivosUV.push(response)
-                })
-        }
-    }
-
-    ngOnDestroy() {
-        this.destroy$.next(true)
-    }
+  ngOnDestroy() {
+    this.destroy$.next(true)
+  }
 }
